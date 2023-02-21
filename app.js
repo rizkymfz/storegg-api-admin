769749b0bc6d5401c7cb0edcfbde0bb0d65603af
rizkymfz <rizky.rmfz@gmail.com>
2023-02-21 13:22:58 +0700

    add comment

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import createError from 'http-errors';
 import session from 'express-session';
 import flash from 'connect-flash';
 
-//router
+//router controller
 import dashboardRouter from "./app/dashboard/router.js";
 import categoryRouter from'./app/category/router.js';
 import nominalRouter from './app/nominal/router.js';
